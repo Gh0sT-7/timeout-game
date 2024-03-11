04abe0e174274af7ff5d33d6a8b4019390946dfc
James Mills <james.mills@lontra.co.uk>
2024-03-11 17:31:18 +0000

    Included createPortal to move the dialog to it's own modal element wrapper outside of the main content.

--- src/components/ResultModal/ResultModal.jsx
+++ src/components/ResultModal/ResultModal.jsx
@@ -1,5 +1,6 @@
 import './ResultModal.scss';
 import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
 	const dialog = useRef();
@@ -16,7 +17,7 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
 		};
 	});
 
-    return (
+    return createPortal(
 		<dialog ref={dialog} className='result-modal' onClose={onReset}>
 			{userLost && <h2>You lost!</h2>}
 			{!userLost && <h2>You score: {score}</h2>}
@@ -29,7 +30,8 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
 			<form method='dialog' onSubmit={onReset}>
 				<button>Close</button>
 			</form>
-		</dialog>
+		</dialog>,
+		document.getElementById('modal')
     );
 });
 
